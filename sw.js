4f2b8fd35cdc87f03f8aa34478580f1b2ccbb361
jake.ko <jake.ko@kakaocorp.com>
2020-05-05 22:42:52 +0900

    pwa 추가

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('video-store').then(function(cache) {
+            return cache.addAll([
+                '/index.html',
+                '/jquery-3.4.1.min.js',
+                '/main.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    // console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
